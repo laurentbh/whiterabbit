2f6c67646e0aaaf09f77d41f2fdf5d5f25ce22d7
laurent buhler <lbuhler@gmail.com>
2020-10-02 12:39:32 -0700

    add basic database functions

---
+++ database.go
@@ -0,0 +1,44 @@
+package whiterabbit
+
+import (
+	"strconv"
+
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+type DB struct {
+	driver neo4j.Driver
+}
+
+const (
+	host      = "localhost"
+	port      = 7687
+	encrypted = false
+	user      = "root"
+	password  = "neo4j"
+)
+
+// Open a connection to neo4j
+// TODO : pass a config
+func Open() (*DB, error) {
+
+	uri := "bolt://" + host + ":" + strconv.Itoa(port)
+	driver, err := neo4j.NewDriver(uri,
+		neo4j.BasicAuth(user, password, ""),
+		func(c *neo4j.Config) {
+			c.Encrypted = encrypted
+		})
+	if err != nil {
+		return nil, err
+	}
+	return &DB{driver: driver}, nil
+}
+
+// GetSession open session
+func (r *DB) GetSession() (neo4j.Session, error) {
+	session, err := r.driver.Session(neo4j.AccessModeWrite)
+	if err != nil {
+		return nil, err
+	}
+	return session, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/laurentbh/whiterabbit
+
+go 1.15
+
+require github.com/neo4j/neo4j-go-driver v1.8.3
