e18da58952877c8d1766aeaa6a030c05d5cd4ee4
laurent buhler <lbuhler@gmail.com>
2020-10-29 15:37:50 -0700

    add test rollback

--- integration/transaction_test.go
+++ integration/transaction_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/neo4j/neo4j-go-driver/neo4j"
@@ -26,7 +27,6 @@ func TestTransactionCommit(t *testing.T) {
 
 			cat1 := Category{Name: "cat 1"}
 			cat2 := Category{Name: "cat 2"}
-			cat3 := Category{Name: "cat 3"}
 
 			var results []neo4j.Result
 
@@ -34,10 +34,35 @@ func TestTransactionCommit(t *testing.T) {
 			results = append(results, r)
 			r, _ = con.CreateNode(cat2)
 			results = append(results, r)
-			r, _ = con.CreateNode(cat3)
-			results = append(results, r)
 
 			return results, nil
 		},
 	)
+	// TODO: test the result
+}
+
+func TestTransactionRollBack(t *testing.T) {
+
+	neo, _ := whiterabbit.Open(whiterabbit.DefaultConfig{})
+	defer neo.Close()
+
+	con, _ := neo.GetConnection()
+	defer con.Close()
+
+	tx := func(con *whiterabbit.Connection) ([]neo4j.Result, error) {
+		cat1 := Category{Name: "cat for rollback 1"}
+		cat2 := Category{Name: "cat for rollback2"}
+
+		var results []neo4j.Result
+
+		r, _ := con.CreateNode(cat1)
+		results = append(results, r)
+		r, _ = con.CreateNode(cat2)
+		results = append(results, r)
+
+		return results, errors.New("force RB")
+	}
+	con.InTransaction(tx)
+	// TODO test results
+
 }
