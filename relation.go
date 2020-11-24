7f15aeae6804e379c3d1f8a34437001cc2bc3229
laurent buhler <lbuhler@gmail.com>
2020-11-24 11:53:58 -0800

    add RelationByNodeID

--- relation.go
+++ relation.go
@@ -1,6 +1,8 @@
 package whiterabbit
 
 import (
+	"strconv"
+
 	"github.com/neo4j/neo4j-go-driver/neo4j"
 )
 
@@ -9,6 +11,40 @@ type Relation struct {
 	Relation string
 	From     interface{}
 	To       interface{}
+}
+
+// RelationByNodeID return all relations for a given node id
+func (con *Connection) RelationByNodeID(id int64, candidates []interface{}) ([]Relation, error) {
+	cypher := "MATCH (n) –[r]-(d) WHERE id(n) = "
+	cypher = cypher + strconv.FormatInt(id, 10)
+	cypher = cypher + " RETURN n,r,d"
+
+	result, err := con.GetSession().Run(cypher,
+		map[string]interface{}{})
+	if err != nil {
+		return nil, err
+	}
+	if err = result.Err(); err != nil {
+		return nil, err
+	}
+	ret := make([]Relation, 0)
+	for result.Next() {
+		var rel Relation
+		record := result.Record()
+
+		rel.From, err = ConvertNode(record.GetByIndex(0).(neo4j.Node), candidates)
+		if err != nil {
+			return ret, err
+		}
+		rel.To, err = ConvertNode(record.GetByIndex(2).(neo4j.Node), candidates)
+		if err != nil {
+			return ret, err
+		}
+		neoRel, _ := record.GetByIndex(1).(neo4j.Relationship)
+		rel.Relation = neoRel.Type()
+		ret = append(ret, rel)
+	}
+	return ret, nil
 }
 
 // MatchRelation ...
