83b9924de2156ba9bf8eb83544706cf3e41c4cca
laurent buhler <lbuhler@gmail.com>
2021-02-01 17:58:41 -0800

    Removed Mapping.Convert func

--- mapping.go
+++ mapping.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
-	"strconv"
 )
 
 // Mapping ...
@@ -17,39 +16,39 @@ type Mapping struct {
 	Model map[string]string
 }
 
-// Convert neo4j record to a struct
-func Convert(target interface{}, props map[string]interface{}, attributes map[string]string) (interface{}, error) {
-	rValue := reflect.ValueOf(target)
-	// fmt.Println("input Kind: ", rValue.Kind())
-	// fmt.Println("input Type: ", rValue.Type())
-	if rValue.Kind() != reflect.Struct {
-		err := errors.New("target is not a struct")
-		return "", err
-	}
-
-	//make a copy
-	copyValuePtr := reflect.New(rValue.Type())
-	// fmt.Println("copy Kind: ", copyValuePtr.Kind()) // => ptr
-	// fmt.Println("copy Type: ", copyValuePtr.Type()) //*(struct)
-
-	// deference
-	copyValue := copyValuePtr.Elem()
-	// fmt.Println("deferenced Kind: ", copyValue.Kind())
-	// fmt.Println("deferenced Type: ", copyValue.Type())
-	// fmt.Println("nb fields : ", copyValue.NumField())
-
-	empty := reflect.Value{}
-	for k, v := range props {
-		fieldVal := copyValue.FieldByName(k)
-		if fieldVal != empty {
-			err := setValue(&fieldVal, attributes[k], v.(string))
-			if err != nil {
-				return "", err
-			}
-		}
-	}
-	return copyValue.Interface(), nil
-}
+//// Convert neo4j record to a struct
+//func Convert(target interface{}, props map[string]interface{}, attributes map[string]string) (interface{}, error) {
+//	rValue := reflect.ValueOf(target)
+//	// fmt.Println("input Kind: ", rValue.Kind())
+//	// fmt.Println("input Type: ", rValue.Type())
+//	if rValue.Kind() != reflect.Struct {
+//		err := errors.New("target is not a struct")
+//		return "", err
+//	}
+//
+//	//make a copy
+//	copyValuePtr := reflect.New(rValue.Type())
+//	// fmt.Println("copy Kind: ", copyValuePtr.Kind()) // => ptr
+//	// fmt.Println("copy Type: ", copyValuePtr.Type()) //*(struct)
+//
+//	// deference
+//	copyValue := copyValuePtr.Elem()
+//	// fmt.Println("deferenced Kind: ", copyValue.Kind())
+//	// fmt.Println("deferenced Type: ", copyValue.Type())
+//	// fmt.Println("nb fields : ", copyValue.NumField())
+//
+//	empty := reflect.Value{}
+//	for k, v := range props {
+//		fieldVal := copyValue.FieldByName(k)
+//		if fieldVal != empty {
+//			err := setValue(&fieldVal, attributes[k], v.(string))
+//			if err != nil {
+//				return "", err
+//			}
+//		}
+//	}
+//	return copyValue.Interface(), nil
+//}
 
 // GetMapping builds a Mapping structure for a given struct
 func GetMapping(input interface{}) (Mapping, error) {
@@ -120,27 +119,34 @@ func getValue(v reflect.Value) (interface{}, error) {
 	msg := fmt.Sprintf("getValue for %v is not implemented", v.Kind())
 	return "", errors.New(msg)
 }
-func setValue(v *reflect.Value, targetType string, value string) error {
-	switch targetType {
-	case "string":
-		v.SetString(value)
-		return nil
-	case "int":
-		intVal, err := strconv.Atoi(value)
-		if err != nil {
-			return err
-		}
-		v.SetInt(int64(intVal))
-		return nil
-	case "float64":
-		floatVal, err := strconv.ParseFloat(value, 64)
-		if err != nil {
-			return err
-		}
-		v.SetFloat(floatVal)
-		return nil
-	default:
-		msg := fmt.Sprintf("setValue for %v is not implemented", targetType)
-		return errors.New(msg)
-	}
-}
+//func setValue(v *reflect.Value, targetType string, value string) error {
+//	switch targetType {
+//	case "string":
+//		v.SetString(value)
+//		return nil
+//	case "int":
+//		intVal, err := strconv.Atoi(value)
+//		if err != nil {
+//			return err
+//		}
+//		v.SetInt(int64(intVal))
+//		return nil
+//	case "float64":
+//		floatVal, err := strconv.ParseFloat(value, 64)
+//		if err != nil {
+//			return err
+//		}
+//		v.SetFloat(floatVal)
+//		return nil
+//	case "slice":
+//		switch v.Type().String() {
+//		case "[]string":
+//			return nil
+//		default:
+//			return nil
+//		}
+//	default:
+//		msg := fmt.Sprintf("setValue for %v is not implemented", targetType)
+//		return errors.New(msg)
+//	}
+//}
